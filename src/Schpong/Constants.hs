9f59fe566548126602cf975d0dea4822ea78214c
barsanges <barsanges@users.noreply.github.com>
2020-09-14 21:55:25 +0200

    Increase rope speed.

--- src/Schpong/Constants.hs
+++ src/Schpong/Constants.hs
@@ -28,7 +28,7 @@ ropeWidth = 2
 
 -- | Speed of the rope on the y axis.
 ropeSpeed :: Float
-ropeSpeed = 2 * yBounceSpeed
+ropeSpeed = 4 * yBounceSpeed
 
 -- | Size of the character.
 characterSize :: (Float, Float)
